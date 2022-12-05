625e4b7e94cf87f84b268e0542bbda88c5fe80fb
shanmukharao369 <shanmukha.pediredla@gmail.com>
2022-12-05 18:20:14 +0530

    task9

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,28 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-  const expenseData = new Date(2022,12,5);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 245.98;
-  const LocationOfExpenditure = 'vizag'
   return (
     
     <div className = "expense-item">
-    <div>{expenseData.toISOString()}</div>
+    <div>{props.item.date.toISOString()}</div>
     <div className="expense-item__description">
-    <h2> {expenseTitle} - {LocationOfExpenditure}</h2>
-    <div className="expense-item__price"> ${expenseAmount}</div>
+    <h2> {props.item.title} - {props.item.locationOfExpenditure} </h2>
+    <div className="expense-item__price"> ${props.item.amount}</div>
     </div>
     </div>
     
     
-    // <div>
-    // <div><h1>Expense Items</h1></div>
-    // <div>Food Rs 10</div>
-    // <div>Petrol Rs 100</div>
-    // <div>Movies Rs 200</div>
-    // </div>
+  
   );
 }
 
