c2c23f25c2cea9880af4ddb4cc53beb924687a0a
shanmukharao369 <shanmukha.pediredla@gmail.com>
2022-12-05 10:45:42 +0530

    recatexpenseItems7

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+
+
+function ExpenseItem() {
+  return (
+    <div>
+    <div><h1>Expense Items</h1></div>
+    <div>Food Rs 10</div>
+    <div>Petrol Rs 100</div>
+    <div>Movies Rs 200</div>
+      
+    </div>
+
+    
+  );
+}
+
+export default ExpenseItem;
